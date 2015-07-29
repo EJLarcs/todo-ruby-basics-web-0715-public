529f468f7896a717aa48783dffac9b6a0c10cb9d
EJLarcs <ejlarcombe@gmail.com>
2015-07-29 15:48:54 -0400

    Add pizza flavours

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+    num1 / num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+  persons_name = name
 end
 
-def argue
-  
+def argue(phrase)
+  phrase = "I'm right and you are wrong!"
 end
 
-def greeting
-
+def greeting(x, y)
+    
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(argument="cheese")
+    argument
 end
